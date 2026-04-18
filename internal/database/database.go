4b46ed866564ef37da3e3ff3ceaa189fde957880
bailangvvkg <wuxiuxiumama@gmail.com>
2026-04-18 11:48:42 +0800

    refactor: switch to pure go sqlite driver

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,24 +2,15 @@ package database
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 
 	"halo/internal/model"
 
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
 func Open(path string) (*gorm.DB, error) {
-	if _, err := os.Stat(path); err != nil && !os.IsNotExist(err) {
-		return nil, fmt.Errorf("stat database: %w", err)
-	}
-
-	if os.Getenv("CGO_ENABLED") == "0" {
-		return nil, fmt.Errorf("sqlite runtime requires cgo-enabled Go environment on this machine")
-	}
-
 	dsn := filepath.ToSlash(filepath.Clean(path)) + "?_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)"
 	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 	if err != nil {
